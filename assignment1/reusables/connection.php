10bd99ef1bf3e61fbaf01a276d5597c37076fc02
Dhruv Shah <dhruvshah2811ds@gmail.com>
2025-03-05 14:36:10 -0500

    connection string changed

--- assignment1/reusables/connection.php
+++ assignment1/reusables/connection.php
@@ -1,14 +1,14 @@
 <?php 
         $connect = mysqli_connect(
             'localhost',
-            'root',
-            '',
-            'http5225assignment1'
+            'u384574077_dhruv',
+            'Shruti28$',
+            'u384574077_phpdhruva1'
         );
 
         if(!$connect){
             echo 'Error Code: ' . mysqli_connect_errno();
             echo 'Error Message: ' . mysqli_connect_error();
             exit;
-        }
+        }
 ?>
